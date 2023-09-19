1c63c92852a373940fbf373c8678a254e1f16318
vlad.galuska <vlad.galuska@hibyte.ro>
2023-09-19 13:54:26 +0300

    First commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        for (var _ : List.of(1, 2, 3)) {
+            System.out.println("Hello, World!");
+        }
+    }
+}
